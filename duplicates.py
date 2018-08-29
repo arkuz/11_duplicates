27963fb4737a6a2698ecae12e3f4280c280091cf
arkuz <arkuzka@mail.ru>
2018-08-29 16:05:33 +0300

    tmp

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,27 @@
+import os
+
 
 if __name__ == '__main__':
-    pass
+    path = os.getcwd()
+
+    print(path)
+
+
+    r = os.walk('/home/arkuz/Devman/GitHub/11_duplicates')
+
+    for i in r:
+        print(i)
+    print('=' * 20)
+
+
+    def crawlDirectories(directoryToCrawl):
+        output = []
+        for path, dirnames, filenames in os.walk(directoryToCrawl):
+            for fname in filenames:
+                output.append(os.path.join(path, fname))
+        return output
+
+    t = crawlDirectories('/home/arkuz/Devman/GitHub/11_duplicates')
+
+    for i in t:
+        print(i)
