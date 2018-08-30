644d95ecb2788da68e505dcb7e2f2c5d3bc51c85
arkuz <arkuzka@mail.ru>
2018-08-30 23:24:11 +0300

    fixed comments

--- duplicates.py
+++ duplicates.py
@@ -6,57 +6,49 @@ from collections import Counter
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dirPath', help='Path to directory')
+    parser.add_argument('dirpath', help='Path to directory')
     return parser
 
 
 def get_files_info_list(directory):
-    full_files_info_list = []
-    short_files_info_list = []
+    files_info_list = {}
     for path, dirnames, filenames in os.walk(directory):
         for filename in filenames:
             filepath = os.path.join(path, filename)
             filesize = os.path.getsize(filepath)
-            full_file_info = (
-                os.path.normpath(filepath),
-                filename,
-                filesize
-            )
-            short_file_info = (
-                filename,
-                filesize
-            )
-            full_files_info_list.append(full_file_info)
-            short_files_info_list.append(short_file_info)
-    return full_files_info_list, short_files_info_list
+            files_info_list.setdefault(
+                (filename, filesize), []).append(filepath)
+    return files_info_list
 
 
-def find_duplicate(short_list):
-    duplicate_list = [
-        dup for dup, count in Counter(short_list).items() if count > 1
-    ]
+def get_duplicate_list(files_info_list):
+    duplicate_list = []
+    for paths in files_info_list.values():
+        if len(paths) > 1:
+            for path in paths:
+                duplicate_list.append(path)
     return duplicate_list
 
 
-def print_duplicate_list(full_files_list, duplicate_list):
+def print_duplicate_list(duplicate_list):
     print('Duplicate files:')
-    for filepath, filename, filesize in full_files_list:
-        for fname, fsize in duplicate_list:
-            if filename == fname and filesize == fsize:
-                print('  {0}'.format(filepath))
+    for path in duplicate_list:
+        print('  {}'.format(path))
 
 
 if __name__ == '__main__':
 
     parser = create_arg_parser()
     args = parser.parse_args()
-    dirpath = os.path.abspath(args.dirPath)
+    dirpath = os.path.abspath(args.dirpath)
 
     if not os.path.isdir(dirpath):
         sys.exit('The path is not a directory.')
 
-    full_files_list, short_files_list = get_files_info_list(dirpath)
+    files_list = get_files_info_list(dirpath)
 
-    duplicate_list = find_duplicate(short_files_list)
+    duplicate_list = get_duplicate_list(files_list)
+    if not duplicate_list:
+        print('Duplicate files not exist.')
 
-    print_duplicate_list(full_files_list, duplicate_list)
+    print_duplicate_list(duplicate_list)
